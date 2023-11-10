33cbc96dca82a58c4f676cfe6dddb84f2be8c38d
Ayrat Ziganshin <zairat@inbox.ru>
2023-11-10 16:10:06 +0300

    WFBO-73 Done

---
+++ src/Request/bannerType/list/Template.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BusinessGazeta\AdfoxApi\Request\bannerType\list;
+
+use BusinessGazeta\AdfoxApi\Request\AbstractAdfoxRequest;
+
+/**
+ * @link https://yandex.ru/dev/adfox/doc/v.1/account/account-list-activeBanners.html
+ */
+
+
+class Template extends AbstractAdfoxRequest
+{
+    private int $objectID;
+
+    public function __construct(int $objectID)
+    {
+        $this->objectID = $objectID;
+    }
+
+    public function params(): array
+    {
+        $params = parent::params();
+        $params = array_merge($params, ['objectID' => $this->objectID]);
+
+        return [
+            'query' => $params
+        ];
+    }
+
+    /**
+     * @return int
+     */
+    public function getObjectID(): int
+    {
+        return $this->objectID;
+    }
+
+    /**
+     * @param int $objectID
+     */
+    public function setObjectID(int $objectID): void
+    {
+        $this->objectID = $objectID;
+    }
+
+
+}
